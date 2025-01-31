2bd4a2f8dbeb264640d4bb3270be34244bf9b24f
adamsaleh11 <sadam1589@gmail.com>
2025-01-31 11:29:38 -0500

    Add getters

--- src/Incident.java
+++ src/Incident.java
@@ -10,5 +10,20 @@ public class Incident {
         this.zoneID = zoneID;
         this.eventType = eventType;
         this.severity = severity;
+        //Apply logic to determine amount of water needed. Could be in data or set based on severity level.
+    }
+    public String getTime() {
+        return this.time;
+    }
+
+    public int getZone() {
+        return this.zoneID;
+    }
+
+    public String getEventType() {
+        return this.eventType;
+    }
+    public String getSeverity() {
+        return this.severity;
     }
 }
