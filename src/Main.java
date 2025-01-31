eb2c2d59d6bb4c8ff6826b2a80ebd9754460bb9b
adamsaleh11 <sadam1589@gmail.com>
2025-01-31 12:50:51 -0500

    Adding functionality to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+/**
+ * This is the main class simulating the beginning of the Drone system simulation. The user is prompted
+ * how many drones they need.
+ */
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        LocalAreaNetwork LAN = new LocalAreaNetwork();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Thank you contacting Fighting Fires hotline. How many drones do you need?\n");
+        int numDrones = scanner.nextInt();
+        for (int i = 0; i < numDrones; i++) {
+          Thread DroneSubSystem = new Thread(new DroneSubsytem(LAN, i));
+          DroneSubSystem.start();
+        }
+        System.out.println("The drone system has been deployed. Waiting on instructions to proceed further.\n");
     }
 }
