2bff6256190128e649eac89eff6d794ccb959507
adamsaleh11 <sadam1589@gmail.com>
2025-02-02 12:48:07 -0500

    Update main class

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,19 @@ public class Main {
      */
     public static void main(String[] args) {
         LocalAreaNetwork LAN = new LocalAreaNetwork();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Thank you contacting Fighting Fires hotline. How many drones do you need?\n");
-        int numDrones = scanner.nextInt();
-        for (int i = 0; i < numDrones; i++) {
-          Thread DroneSubSystem = new Thread(new DroneSubsytem(LAN, i));
-          DroneSubSystem.start();
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Thank you contacting Fighting Fires hotline. How many drones do you need?\n");
+////        int numDrones = scanner.nextInt();
+//        for (int i = 0; i < numDrones; i++) {
+//          Thread DroneSubSystem = new Thread(new DroneSubsytem(LAN, i));
+//          DroneSubSystem.start();
+//        }
+        Thread scheduler = new Thread(new Scheduler(LAN));
+        Thread fireIncidentSystem = new Thread(new FireIncidentSubsystem(LAN, "Sample_event_file.csv"));
+        Thread droneSubsystem = new Thread(new DroneSubsytem(LAN, 1));
+        scheduler.start();
+        fireIncidentSystem.start();
+        droneSubsystem.start();
         System.out.println("The drone system has been deployed. Waiting on instructions to proceed further.\n");
     }
 }
