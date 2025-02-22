a894a63ee4eab0cdaa1539430ee771f65314d114
adamsaleh11 <sadam1589@gmail.com>
2025-02-22 18:57:46 -0500

    Update variables

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,8 @@ public class Main {
         List<Zone> zones = loadZones("src/resources/Sample_zone_file.csv");
         System.out.println("Loaded zones: " + zones.size());
         LAN.setZones(zones);
-
         DroneSubsytem drone = new DroneSubsytem(LAN, 1);
         LAN.addDrone(drone);
-
         Thread scheduler = new Thread(new Scheduler(LAN));
         Thread fireIncidentSystem = new Thread(new FireIncidentSubsystem(LAN, "src/resources/Sample_event_file.csv"));
         Thread droneSubsystem = new Thread(drone);
@@ -38,16 +36,16 @@ public class Main {
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
                 int zoneID = Integer.parseInt(parts[0].trim());
-                String startStr = parts[1].trim();
-                startStr = startStr.substring(1, startStr.length() - 1);
-                String[] startCoords = startStr.split(";");
-                int startX = Integer.parseInt(startCoords[0].trim());
-                int startY = Integer.parseInt(startCoords[1].trim());
-                String endStr = parts[2].trim();
-                endStr = endStr.substring(1, endStr.length() - 1);
-                String[] endCoords = endStr.split(";");
-                int endX = Integer.parseInt(endCoords[0].trim());
-                int endY = Integer.parseInt(endCoords[1].trim());
+                String startCoords = parts[1].trim();
+                startCoords = startCoords.substring(1, startCoords.length() - 1);
+                String[] startingCoords = startCoords.split(";");
+                int startX = Integer.parseInt(startingCoords[0].trim());
+                int startY = Integer.parseInt(startingCoords[1].trim());
+                String endCoords = parts[2].trim();
+                endCoords = endCoords.substring(1, endCoords.length() - 1);
+                String[] endingCoords = endCoords.split(";");
+                int endX = Integer.parseInt(endingCoords[0].trim());
+                int endY = Integer.parseInt(endingCoords[1].trim());
                 zones.add(new Zone(zoneID, startX, startY, endX, endY));
             }
         } catch (IOException e) {
